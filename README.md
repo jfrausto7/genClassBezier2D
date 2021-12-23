566f511fb90b15c5d8e3721a616436677d796eec
jfrausto7 <jacob_frausto@brown.edu>
2021-12-22 23:02:11 -0700

    created generate_random_shape
    This function utilizes prior work that's credited in the README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # genclass3D
  A short "starter project" with the goal of generating a small dataset of abstract 3D shapes and then training a model to classify them.
+
+### Credits 
+
+This application uses Open Source components. You can find the source code of their open source projects along with license information below. We acknowledge and are grateful to these developers for their contributions to open source.
+
+Project: bezier_shapes https://github.com/jviquerat/bezier_shapes
+Copyright (c) 2019 Jonathan Viquerat (marc@imadjine.com)
+License (MIT) https://github.com/jviquerat/bezier_shapes/blob/master/LICENSE
---
+++ generate_random_shape.py
@@ -0,0 +1,31 @@
+from shapes_utils import *
+
+# constraints
+plot_pts       = True
+magnify        = 1.5
+xmin           =-1.0
+xmax           = 1.0
+ymin           =-1.0
+ymax           = 1.0
+
+def generate(filename, n_pts, n_sampling_pts, radius=0.5, edgy=1.0, fillColor='red', hatch=''):
+  # generate shape and write to csv
+  shape = Shape(filename,
+              None,
+              n_pts,
+              n_sampling_pts,
+              [radius],
+              [edgy],
+              fillColor,
+              hatch)
+  shape.generate(magnify = magnify)
+  shape.generate_image(plot_pts = plot_pts,
+                      xmin     = xmin,
+                      xmax     = xmax,
+                      ymin     = ymin,
+                      ymax     = ymax)
+  shape.write_csv()
+
+  return shape.name
+
+generate('shape', 5, 50)
