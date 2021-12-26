bd60a9b7c8157e6af3786c8a770c497af6934f4b
jfrausto7 <jacob_frausto@brown.edu>
2021-12-26 01:42:21 -0700

    fixed dataset selection

--- main.py
+++ main.py
@@ -34,6 +34,13 @@ def parse_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
+        '--dataset',
+        help='Select a dataset to use',
+        default='shapes',
+        dest='dataset'
+    )
+
+    parser.add_argument(
         '--generate',
         help='Generate dataset of abstract shapes from Bezier curves',
         action='store_true',
@@ -176,38 +183,41 @@ def main(args: argparse.Namespace) -> None:
 
     ## load respective datasets
 
-    # shapes only
-    shape_train_ds, shape_valid_ds, shape_test_ds = create_datasets(shapes_dir)
-    assert_class_num_equiv(shape_train_ds, shape_valid_ds, shape_test_ds)   # ensure that these datasets have same class number
+    if args.dataset == 'shapes':
+        # shapes only
+        shape_train_ds, shape_valid_ds, shape_test_ds = create_datasets(shapes_dir)
+        assert_class_num_equiv(shape_train_ds, shape_valid_ds, shape_test_ds)   # ensure that these datasets have same class number
+        # augment data with cv2
+        train_ds, val_ds, test_ds = augment_dataset(shape_train_ds), augment_dataset(shape_valid_ds), augment_dataset(shape_test_ds)
 
-    # shapes w/ textures
-    texture_train_ds, texture_valid_ds, texture_test_ds = create_datasets(textures_dir)
-    assert_class_num_equiv(texture_train_ds, texture_valid_ds, texture_test_ds) # ensure that these datasets have same class number
+    elif args.dataset == 'textures':
+        # shapes w/ textures
+        texture_train_ds, texture_valid_ds, texture_test_ds = create_datasets(textures_dir)
+        assert_class_num_equiv(texture_train_ds, texture_valid_ds, texture_test_ds) # ensure that these datasets have same class number
+        # augment data with cv2
+        train_ds, val_ds, test_ds = augment_dataset(texture_train_ds), augment_dataset(texture_valid_ds), augment_dataset(texture_test_ds)
 
-    # shapes w/ textures & colors
-    color_train_ds, color_valid_ds, color_test_ds = create_datasets(colors_dir)
-    assert_class_num_equiv(color_train_ds, color_valid_ds, color_test_ds)   # ensure that these datasets have same class number
-
-    # augment data with cv2
-    shape_train_ds, shape_valid_ds, shape_test_ds = augment_dataset(shape_train_ds), augment_dataset(shape_valid_ds), augment_dataset(shape_test_ds)
-    texture_train_ds, texture_valid_ds, texture_test_ds = augment_dataset(texture_train_ds), augment_dataset(texture_valid_ds), augment_dataset(texture_test_ds)
-    color_train_ds, color_valid_ds, color_test_ds = augment_dataset(color_train_ds), augment_dataset(color_valid_ds), augment_dataset(color_test_ds)
-
-    # print_dataset(shape_train_ds)
+    elif args.dataset == 'colors':
+        # shapes w/ textures & colors
+        color_train_ds, color_valid_ds, color_test_ds = create_datasets(colors_dir)
+        assert_class_num_equiv(color_train_ds, color_valid_ds, color_test_ds)   # ensure that these datasets have same class number
+        # augment data with cv2
+        train_ds, val_ds, test_ds = augment_dataset(color_train_ds), augment_dataset(color_valid_ds), augment_dataset(color_test_ds)
+    
+    else:
+        print("Dataset specified is invalid.")
+        return
 
     #instantiate model
     bezierModel = BezierModel()
 
     if args.train:
         # train the model
-        bezierModel = train(bezierModel, shape_train_ds, shape_valid_ds, args.epochs, args.weights)
+        bezierModel = train(bezierModel, train_ds, val_ds, args.epochs, args.weights)
     
     if args.test:
         # test the model
-        test(bezierModel, shape_test_ds)
-
-    # TODO: other datasets!
-
+        test(bezierModel, test_ds)
 
 if __name__ == "__main__":
   args = parse_args()
