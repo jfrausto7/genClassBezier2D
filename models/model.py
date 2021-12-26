d20155a551cc6f8970168d6cd3fd616ce49f89ac
jfrausto7 <jacob_frausto@brown.edu>
2021-12-25 18:25:06 -0700

    set up initial preliminary methods for model

--- models/model.py
+++ models/model.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.layers import \
     Conv2D, MaxPool2D, Dropout, Flatten, Dense, GlobalMaxPool2D
+import numpy as np
+from typing import Counter
 
 #TODO: set up hyperparams
 NUM_OUTPUTS = 3
@@ -40,10 +42,30 @@ class BezierModel:
     # show summary of model architecture
     self.model.summary()
   
-  def train(self, dataset):
+  def train(self, dataset, epochs, ckpt_callback, tb_callback):
     """ TODO: Train the model on input images with their corresponding shape labels """
-    pass
+    train_result = self.model.fit(dataset, epochs=epochs, callbacks=[ckpt_callback, tb_callback])
+    accuracy = train_result.history['categorical_accuracy'][-1]
+    loss = train_result.history['loss'][-1]
+    return accuracy, loss
+
+  def validate(self, dataset):
+    """ TODO: Validate the model on unseen input images with their corresponding shape labels """
+    valid_result = self.model.evaluate(dataset)
+    accuracy = valid_result.history['categorical_accuracy'][-1]
+    loss = valid_result.history['loss'][-1]
+    return accuracy, loss
 
   def test(self, dataset):
     """ TODO: Test the model on unseen input images with their corresponding shape labels """
-    pass
+    preds = self.model.predict(dataset)
+
+    # get the most occurring class
+    classes = []
+    for pred in preds:
+      classes.append(np.argmax(pred, axis=0))
+    counts = Counter(classes)
+    predictions = counts.most_common()
+    print(predictions)
+    prediction = counts.most_common()[0][0]
+    return prediction
